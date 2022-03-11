502db878a29dd6dfefaa4d0c806f01b0d0dcf341
TranGiaPhuc <mtilow2277@gmai.com>
2022-03-11 15:10:27 +0700

    commit

--- KiemTraNe/KiemTraNe/Form1.cs
+++ KiemTraNe/KiemTraNe/Form1.cs
@@ -16,5 +16,10 @@ namespace KiemTraNe
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
